c7a4f3c2ca350fbf1e1f4ba9509b3c059c4b73a0
sxmeer <sameeryadav2421@gmail.com>
2020-05-23 13:57:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = 3200;
+const app = express();
+app.listen(3200,()=>{
+    console.log(`server is connected at port ${PORT}`);
+});
+app.get('/',(req,res)=>{
+    res.send({
+        msg: 'page you\'ve requested is not found'
+    });
+})
