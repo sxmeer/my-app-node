b168fdb9b91baa0e328d81f5ee5b909c3565f24c
sxmeer <sameeryadav2421@gmail.com>
2020-05-24 10:08:14 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./model/user');
 
@@ -14,6 +15,25 @@ mongoose.connect('mongodb+srv://sam:iwtbiG@5@cluster0-c9u0q.mongodb.net/test?ret
 app.listen(PORT,()=>{
     console.log(`server is connected at port ${PORT}`);
 });
+
+
+app.use(cors({ origin: '*' }));
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+
 app.get('/',(req,res)=>{
     var user = new User({
         name: 'sameer',
