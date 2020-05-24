6de902aaa3f3207f3fddb70af6a462940a910a00
sxmeer <sameeryadav2421@gmail.com>
2020-05-24 10:14:26 +0530

    s

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ app.listen(PORT,()=>{
 });
 
 
-app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
 
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//   );
+//   next();
+// });
 
 
 app.get('/',(req,res)=>{
